7872d1c5ce90de1c33a155196bd25ee31e59e00a
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2021-07-08 01:15:47 +0530

    line comments vs block comments

---
+++ getting-started/comments.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+
+using namespace std;
+
+int main() {
+    // A single line comment mentioning something in brief
+    cout << "Comments are cool!";
+
+    // putting a line break
+    cout << "\n";
+
+    /*
+     * A block comment mentioning something in detail
+     * 1. foo
+     * 2. bar
+     * 3. baz
+     * */
+}
