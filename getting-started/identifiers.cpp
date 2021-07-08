a0b66bc137fb9e211a4878cac43e4a741549a676
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2021-07-09 01:00:58 +0530

    identifier rules

---
+++ getting-started/identifiers.cpp
@@ -0,0 +1,16 @@
+int main() {
+    int foo = 10;
+    int _foo = 20;
+    int FOO = 30;
+    int Foo99 = 99;
+
+    /*
+     * Rules for variable naming
+     * 1. Only have [A-Z] or [a-z] or numbers [0-9] or _ (underscore)
+     * 2. Can only begin with `_` or alphabet
+     * 3. Case sensitive
+     * 4. Cannot contain space
+     * 5. Cannot use keywords as identifiers
+     * */
+}
+
