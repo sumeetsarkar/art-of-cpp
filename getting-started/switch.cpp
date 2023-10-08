41e30255ebd3ee030912a814b4f3bd632025acef
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2023-10-08 11:50:27 +0530

    [[fallthrough]] attribute

--- getting-started/switch.cpp
+++ getting-started/switch.cpp
@@ -2,13 +2,20 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-  int val = 101;
+  int val = 400;
   switch (val) {
     case 10:
       std::cout << val;
       break;
     case 100:
+      [[fallthrough]];
     case 200:
+      [[fallthrough]];
+    case 300:
+      [[fallthrough]];
+    case 400:
+      [[fallthrough]];
+    case 500:
       std::cout << val;
       break;
     default:
