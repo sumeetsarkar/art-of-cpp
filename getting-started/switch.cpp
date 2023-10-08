435094d0bbce2de500d39d9e1db42e05abfec1f3
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2023-10-08 12:14:04 +0530

    switch block var declaration

--- getting-started/switch.cpp
+++ getting-started/switch.cpp
@@ -4,7 +4,11 @@
 int main(int argc, char **argv) {
   int val = 400;
   switch (val) {
+    int x;
+    int y;
+    // int z = 10; // initialization not allowed
     case 10:
+      y = 20;  // initialize in case block
       std::cout << val;
       break;
     case 100:
